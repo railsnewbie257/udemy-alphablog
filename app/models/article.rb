6678de091260c074375a6bc7356f6bf3ff5de333
railsnewbie257 <railsnewbie257@gmail.com>
2017-05-06 17:10:00 -0500

    show, create, routes

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,4 @@
 class Article < ActiveRecord::Base
   validates :title, presence: true, length: {minimum:3, maximum:20}
+  validates :description, presence: true, length: {minimum:10, maximum:30}
 end
