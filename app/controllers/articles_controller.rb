b10ea0a6887bdc8ad64fc44b267d2a16d2b5359f
railsnewbie257 <railsnewbie257@gmail.com>
2017-05-06 20:03:42 -0500

    partials and refactor

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-
+before_action :set_article, only: [:edit, :update, :show, :destroy]
   def index
     @articles = Article.all
   end
@@ -9,11 +9,9 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def create
@@ -27,7 +25,6 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice] = "Article was sucessfully update"
       redirect_to article_path(@article)
@@ -36,8 +33,22 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def destroy
+    if @article.destroy
+      flash[:notice]="Article was sucessfully deleted"
+    else
+      flash[:danger]="Deletion failed"
+    end
+    redirect_to articles_path
+
+  end
+
 
   private
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
     def article_params
       params.require(:article).permit(:title, :description)
     end
