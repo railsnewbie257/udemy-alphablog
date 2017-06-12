b2aa940ef08344f37a10256056b41519106c1a5d
railsnewbie257 <railsnewbie257@gmail.com>
2017-06-12 12:53:33 -0500

    fixe tests and update nav nar with categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,5 @@
 class CategoriesController < ApplicationController
-
-  def show
-    @categories=Category.find(params[:id])
-  end
+before_action :require_admin, except: [:index, :show]
 
   def index
     logger.debug "----- Category index -----"
@@ -12,6 +9,8 @@ class CategoriesController < ApplicationController
   def new
     @category = Category.new
   end
+
+
 
   def create
     @category = Category.new(category_params)
@@ -23,8 +22,19 @@ class CategoriesController < ApplicationController
     end
   end
 
+  def show
+    @categories=Category.find(params[:id])
+  end
+
   private
     def category_params
       params.require(:category).permit(:name)
+    end
+
+    def require_admin
+      if !logged_in? || (logged_in? and !current_user.admin?)
+        flash[:danger] = 'Only admin can perform that action'
+        redirect_to categories_path
+      end
     end
 end
--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -2,23 +2,32 @@ require 'test_helper'
 
 class CategoriesControllerTest < ActionController::TestCase
 
-def setup
-  @category = Category.create(name: 'sports')
-end
+  def setup
+    @category = Category.create(name: 'sports')
+    @user = User.create(username: 'John', email: 'john@example.com', password: 'password', admin: true)
+  end
 
-test "should get index" do
-  get :index
-  assert_response :success
-end
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
 
-test "should get new" do
-  get :new
-  assert_response :success
-end
+  test "should get new" do
+    session[:user_id] = @user.id
+    # sign_in_as(@user, "password") - this does not work since needs ActionDispatch
+    get :new
+    assert_response :success
+  end
 
-test "should get show" do
-  get(:show, params: {'id' => @category.id})
-  assert_response :success
-end
+  test "should get show" do
+    get(:show, params: {'id' => @category.id})
+    assert_response :success
+  end
 
+  test "should redirect when admin not logged in" do
+    assert_no_difference 'Category.count' do
+      post :create, category: { name: 'sports'}
+    end
+    assert_redirected_to categories_path
+  end
 end
