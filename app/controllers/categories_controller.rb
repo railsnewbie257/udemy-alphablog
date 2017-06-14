42fda0d612bc3547b91d380ebca236fee4546bac
railsnewbie257 <railsnewbie257@gmail.com>
2017-06-14 12:22:16 -0500

    add category functions

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -22,9 +22,23 @@ before_action :require_admin, except: [:index, :show]
     end
   end
 
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:success] = "Category name was successfully update."
+      redirect_to category_path(@category)
+    else
+      render 'edit'
+    end
+  end
+
   def show
     @category=Category.find(params[:id])
-    @category_articles = @category.articles.paginate(page: params[:page], per_page:5)
+    @category_articles = @category.articles #.paginate(page: params[:page], per_page:5)
   end
 
   private
