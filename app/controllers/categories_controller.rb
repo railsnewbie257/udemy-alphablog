53a53cc33ac58598253f325e32e634bb5662acd3
railsnewbie257 <railsnewbie257@gmail.com>
2017-06-14 12:43:03 -0500

    add edit category feature

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -22,6 +22,11 @@ before_action :require_admin, except: [:index, :show]
     end
   end
 
+  def show
+    @category=Category.find(params[:id])
+    @category_articles = @category.articles.paginate(page: params[:page], per_page:5)
+  end
+
   def edit
     @category = Category.find(params[:id])
   end
@@ -29,17 +34,13 @@ before_action :require_admin, except: [:index, :show]
   def update
     @category = Category.find(params[:id])
     if @category.update(category_params)
-      flash[:success] = "Category name was successfully update."
+      flash[:success] = "Category name was sucessfully updated."
       redirect_to category_path(@category)
     else
       render 'edit'
     end
   end
 
-  def show
-    @category=Category.find(params[:id])
-    @category_articles = @category.articles #.paginate(page: params[:page], per_page:5)
-  end
 
   private
     def category_params
